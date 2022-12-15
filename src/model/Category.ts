cf0aba2d41d643bd5dc69e1073368ef01a51d412
Luis Meirelles <luis.gustavo@essentia.com.br>
2022-12-15 08:54:44 -0300

    finaliza a tipagem das categories

--- src/model/Category.ts
+++ src/model/Category.ts
@@ -1,8 +1,11 @@
-interface Category {
-  id: string;
-  name: string;
-  description: string;
-  created_at: Date;
+import { v4 as uuidv4 } from 'uuid';
+
+class Category {
+  constructor(public name: string, public description: string, public created_at: Date, public id?: string) {
+    if (!id) {
+      this.id = uuidv4();
+    }
+  }
 }
 
 export { Category };
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,5 +1,4 @@
 import { Router } from 'express';
-import { v4 as uuidv4 } from 'uuid';
 import { Category } from '../model/Category';
 
 const categoriesRoutes = Router();
@@ -9,12 +8,9 @@ const categories: Category[] = [];
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
-  const category = {
-    id: uuidv4(),
-    name,
-    description,
-    created_at: new Date()
-  };
+  const createdAt = new Date();
+
+  const category = new Category(name, description, createdAt);
 
   categories.push(category);
 
