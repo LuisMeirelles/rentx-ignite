c49f3d53aa8a09526cedeaa015ac09ad5f4d25c3
Luis Meirelles <luis.gustavo@essentia.com.br>
2022-12-20 10:11:50 -0300

    implementa a gravação dos dados do arquivo enviado

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,52 @@
+import fs from 'fs';
+import { parse as csvParse } from 'csv-parse';
+
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
+
+interface IImportCategory {
+  name: string;
+  description: string;
+}
+
 class ImportCategoryUseCase {
-  execute(file: Express.Multer.File) {
-    console.log(file);
+  constructor(private categoriesRepository: ICategoriesRepository) {}
+
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    const stream = fs.createReadStream(file.path);
+
+    const categories: IImportCategory[] = [];
+
+    const parseFile = csvParse();
+
+    stream.pipe(parseFile);
+
+    return new Promise((resolve, reject) => {
+      parseFile
+        .on('data', (line) => {
+          const [name, description] = line;
+
+          categories.push({
+            name,
+            description
+          });
+        })
+        .on('end', () => resolve(categories))
+        .on('error', (error) => reject(error));
+    });
+  }
+
+  async execute(file: Express.Multer.File) {
+    const categories = await this.loadCategories(file);
+
+    categories.map((category) => {
+      const { name, description } = category;
+
+      const categoryExists = this.categoriesRepository.findByName(name);
+
+      if (!categoryExists) {
+        this.categoriesRepository.create(category);
+      }
+    });
   }
 }
 
